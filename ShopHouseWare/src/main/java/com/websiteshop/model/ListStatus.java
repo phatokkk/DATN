fe2cba12b52fb8aa819f945eb921cf252ba0384b
phatokkk <phathhpc04272@fpt.edu.vn>
2024-08-03 11:33:49 +0700

    dùng lombok để tạo allargsConstroctor

--- ShopHouseWare/src/main/java/com/websiteshop/model/ListStatus.java
+++ ShopHouseWare/src/main/java/com/websiteshop/model/ListStatus.java
@@ -8,21 +8,14 @@ import java.io.Serializable;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 public class ListStatus implements Serializable {
 	private static final long serialVersionUID = 1L;
 	private int id;
 	private String status;
 	private Long orderId;
 	
-	public ListStatus() {
-		super();
-	}
-	public ListStatus(int id, String status, Long orderId) {
-		super();
-		this.id = id;
-		this.status = status;
-		this.orderId = orderId;
-	}
+
 	public int getId() {
 		return id;
 	}
